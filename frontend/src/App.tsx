9b63f2856bc75871c409559b51cce1ded8d8399b
Sugam Ghising <ghisingsugam04@gmail.com>
2026-06-04 20:50:41 +0545

    feat: Add NotFound page and integrate it into routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CaseStudy from "./pages/CaseStudy";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:id" element={<CaseStudy />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
